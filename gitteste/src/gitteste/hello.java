727fa04a8975c5b7462332631f79d542e0d10b8f
marcelio <mrinfor2015@gmail.com>
2017-02-02 19:17:37 -0300

    nova mudança

--- gitteste/src/gitteste/hello.java
+++ gitteste/src/gitteste/hello.java
@@ -3,7 +3,8 @@ package gitteste;
 public class hello {
 
 	public static void main(String[] args) {
-		System.out.println("Hello!");
+		System.out.println("Hello ");
+		System.out.println("Word!");
 	}
 
 }
