ef79c2bf2f6d46fcec405274344a4b78e5c2667a
Michell Jacks de Oliveira <michelljacks@gmail.com>
2017-02-02 19:19:08 -0300

    Criado método Hello().

--- gitteste/src/gitteste/hello.java
+++ gitteste/src/gitteste/hello.java
@@ -1,9 +1,12 @@
 package gitteste;
 
 public class hello {
+	public static hello(){
+		System.out.println("Hello!");
+	}
 
 	public static void main(String[] args) {
-		System.out.println("Hello!");
+		hello();
 	}
 
 }
